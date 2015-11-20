eec0e115e340009316182d0bd46c17538bb692c9
NorbiPeti <szatmari.norbert.peter@gmail.com>
2015-11-20 20:36:49 +0100

    Commit

---
+++ Tömbrendező/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Tömbrendező
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.countNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pausebtn = new System.Windows.Forms.Button();
+            this.resumebtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labeli = new System.Windows.Forms.Label();
+            this.labelcs = new System.Windows.Forms.Label();
+            this.labelj = new System.Windows.Forms.Label();
+            this.labelcsere = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.countNumUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // countNumUpDown
+            // 
+            this.countNumUpDown.Location = new System.Drawing.Point(109, 10);
+            this.countNumUpDown.Name = "countNumUpDown";
+            this.countNumUpDown.Size = new System.Drawing.Size(120, 20);
+            this.countNumUpDown.TabIndex = 0;
+            this.countNumUpDown.ValueChanged += new System.EventHandler(this.countNumUpDown_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Elemek száma:";
+            // 
+            // startbtn
+            // 
+            this.startbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.startbtn.Location = new System.Drawing.Point(16, 493);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(75, 23);
+            this.startbtn.TabIndex = 2;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numericUpDown1.Location = new System.Drawing.Point(137, 456);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 458);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sebesség (ms/művelet):";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(484, 250);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(344, 261);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Csere változó";
+            // 
+            // pausebtn
+            // 
+            this.pausebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pausebtn.Location = new System.Drawing.Point(101, 493);
+            this.pausebtn.Name = "pausebtn";
+            this.pausebtn.Size = new System.Drawing.Size(75, 23);
+            this.pausebtn.TabIndex = 7;
+            this.pausebtn.Text = "Szünet";
+            this.pausebtn.UseVisualStyleBackColor = true;
+            this.pausebtn.Click += new System.EventHandler(this.pausebtn_Click);
+            // 
+            // resumebtn
+            // 
+            this.resumebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.resumebtn.Location = new System.Drawing.Point(182, 493);
+            this.resumebtn.Name = "resumebtn";
+            this.resumebtn.Size = new System.Drawing.Size(75, 23);
+            this.resumebtn.TabIndex = 8;
+            this.resumebtn.Text = "Folytatás";
+            this.resumebtn.UseVisualStyleBackColor = true;
+            this.resumebtn.Click += new System.EventHandler(this.resumebtn_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 46.55172F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 53.44828F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel1.Controls.Add(this.labelcsere, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelj, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelcs, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labeli, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(278, 250);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 79);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(9, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "i";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "cs";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(97, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(9, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "j";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(146, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "csere";
+            // 
+            // labeli
+            // 
+            this.labeli.AutoSize = true;
+            this.labeli.Location = new System.Drawing.Point(3, 39);
+            this.labeli.Name = "labeli";
+            this.labeli.Size = new System.Drawing.Size(0, 13);
+            this.labeli.TabIndex = 10;
+            // 
+            // labelcs
+            // 
+            this.labelcs.AutoSize = true;
+            this.labelcs.Location = new System.Drawing.Point(47, 39);
+            this.labelcs.Name = "labelcs";
+            this.labelcs.Size = new System.Drawing.Size(0, 13);
+            this.labelcs.TabIndex = 10;
+            // 
+            // labelj
+            // 
+            this.labelj.AutoSize = true;
+            this.labelj.Location = new System.Drawing.Point(97, 39);
+            this.labelj.Name = "labelj";
+            this.labelj.Size = new System.Drawing.Size(0, 13);
+            this.labelj.TabIndex = 10;
+            // 
+            // labelcsere
+            // 
+            this.labelcsere.AutoSize = true;
+            this.labelcsere.Location = new System.Drawing.Point(146, 39);
+            this.labelcsere.Name = "labelcsere";
+            this.labelcsere.Size = new System.Drawing.Size(0, 13);
+            this.labelcsere.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 528);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.resumebtn);
+            this.Controls.Add(this.pausebtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.countNumUpDown);
+            this.Name = "Form1";
+            this.Text = "Tömbrendező";
+            ((System.ComponentModel.ISupportInitialize)(this.countNumUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown countNumUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button pausebtn;
+        private System.Windows.Forms.Button resumebtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelcsere;
+        private System.Windows.Forms.Label labelj;
+        private System.Windows.Forms.Label labelcs;
+        private System.Windows.Forms.Label labeli;
+    }
+}
+
---
+++ Tömbrendező/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tömbrendező
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Timer = new Timer();
+            Timer.Tick += delegate
+            {
+                startbtn.Enabled = !(Timer.Enabled = Enum.MoveNext()); //Ha végzett, engedélyezi a Start gombot, az időzítőt pedig leállítja
+            };
+        }
+
+        private List<NumericUpDown> NumUpdowns = new List<NumericUpDown>();
+        private void countNumUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            NumUpdowns.ForEach(a => this.Controls.Remove(a));
+            NumUpdowns.Clear();
+            var rand = new Random();
+            for (int i = 0; i < countNumUpDown.Value; i++)
+            {
+                var numupdown = new NumericUpDown();
+                numupdown.Maximum = 1000;
+                numupdown.Value = rand.Next((int)numupdown.Minimum, (int)numupdown.Maximum);
+                numupdown.Location = new Point(10 + i * (numupdown.Width + 10), 50);
+                numupdown.BringToFront();
+                NumUpdowns.Add(numupdown);
+                this.Controls.Add(numupdown);
+            }
+        }
+
+        private IEnumerator Enum;
+        private Timer Timer;
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            /*foreach (bool b in Rendezés())
+                ;*/
+            /*if (Enum == null || !Enum.MoveNext())
+                Enum = Rendezés().GetEnumerator();*/
+            Enum = Rendezés().GetEnumerator();
+            Timer.Interval = (int)numericUpDown1.Value / 10;
+            Timer.Start();
+        }
+
+        /// <summary>
+        /// Gyorsított buborékos rendezés
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable Rendezés()
+        {
+            Point p;
+            NumUpdowns.ForEach(numupdown => numupdown.ReadOnly = true);
+            countNumUpDown.ReadOnly = true;
+
+            HighlightCode("int i = NumUpdowns.Count - 1;");
+            int i = NumUpdowns.Count - 1;
+            labeli.Text = i.ToString();
+            labeli.ForeColor = Color.Red;
+            for (int x = 0; x < 10; x++)
+                yield return null;
+            labeli.ForeColor = Color.Black;
+            HighlightCode("while (i >= 1)");
+            for (int x = 0; x < 10; x++)
+                yield return null;
+            while (i >= 1)
+            {
+                HighlightCode("int cs = 0;");
+                int cs = 0;
+                labelcs.Text = cs.ToString();
+                labelcs.ForeColor = Color.Red;
+                for (int x = 0; x < 10; x++)
+                    yield return null;
+                labelcs.ForeColor = Color.Black;
+                HighlightCode("for (int j = 0; j < i; j++)");
+                for (int j = 0; j < i; j++)
+                {
+                    labelj.Text = j.ToString();
+                    labelj.ForeColor = Color.Red;
+                    for (int x = 0; x < 10; x++)
+                        yield return null;
+                    labelj.ForeColor = Color.Black;
+                    labelcs.ForeColor = Color.Black;
+                    HighlightCode("if (T[j] > T[j + 1])");
+                    if (NumUpdowns[j].Value > NumUpdowns[j + 1].Value)
+                    {
+                        int xtáv = 100 - NumUpdowns[j].Location.X;
+                        int ytáv = 280 - NumUpdowns[j].Location.Y;
+                        Point eredetipoz = NumUpdowns[j].Location;
+                        HighlightCode("int csere = T[j];");
+                        labelcsere.Text = NumUpdowns[j].Value.ToString(); //==csere
+                        labelcsere.ForeColor = Color.Red;
+                        for (int k = 0; k < 10; k++)
+                        {
+                            p = NumUpdowns[j].Location;
+                            p.Offset(xtáv / 10, ytáv / 10);
+                            NumUpdowns[j].Location = p;
+                            yield return null;
+                        }
+                        NumUpdowns[j].Location = new Point(100, 280);
+                        labelcsere.ForeColor = Color.Black;
+                        HighlightCode("T[j] = T[j + 1];");
+                        for (int k = 0; k < 10; k++)
+                        {
+                            p = NumUpdowns[j + 1].Location;
+                            p.Offset(0, 5);
+                            NumUpdowns[j + 1].Location = p;
+                            yield return null;
+                        }
+                        xtáv = eredetipoz.X - NumUpdowns[j + 1].Location.X;
+                        ytáv = eredetipoz.Y - NumUpdowns[j + 1].Location.Y;
+                        for (int k = 0; k < 10; k++)
+                        {
+                            p = NumUpdowns[j + 1].Location;
+                            p.Offset(xtáv / 10, ytáv / 10);
+                            NumUpdowns[j + 1].Location = p;
+                            yield return null;
+                        }
+                        HighlightCode("T[j + 1] = csere;");
+                        xtáv = eredetipoz.X + NumUpdowns[j + 1].Width + 10 - NumUpdowns[j].Location.X;
+                        ytáv = eredetipoz.Y - NumUpdowns[j].Location.Y;
+                        for (int k = 0; k < 10; k++)
+                        {
+                            p = NumUpdowns[j].Location;
+                            p.Offset(xtáv / 10, ytáv / 10);
+                            NumUpdowns[j].Location = p;
+                            yield return null;
+                        }
+                        NumUpdowns[j].Location = new Point(eredetipoz.X + NumUpdowns[j + 1].Width + 10, eredetipoz.Y);
+
+                        var csere = NumUpdowns[j];
+                        NumUpdowns[j] = NumUpdowns[j + 1];
+                        NumUpdowns[j + 1] = csere;
+                        yield return null;
+
+                        HighlightCode("cs = j;");
+                        cs = j;
+                        labelcs.Text = cs.ToString();
+                        labelcs.ForeColor = Color.Red;
+                    }
+                }
+                for (int x = 0; x < 10; x++)
+                    yield return null;
+                labelcs.ForeColor = Color.Black;
+                HighlightCode("i = cs;");
+                i = cs;
+                labeli.Text = i.ToString();
+                labeli.ForeColor = Color.Red;
+                for (int x = 0; x < 10; x++)
+                    yield return null;
+                labeli.ForeColor = Color.Black;
+            }
+            NumUpdowns.ForEach(numupdown => numupdown.ReadOnly = false);
+            countNumUpDown.ReadOnly = false;
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionColor = richTextBox1.ForeColor;
+            richTextBox1.DeselectAll();
+            labeli.Text = "";
+            labelcs.Text = "";
+            labelj.Text = "";
+            labelcsere.Text = "";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (Timer != null)
+            {
+                bool started = Timer.Enabled;
+                Timer.Stop();
+                Timer.Interval = (int)numericUpDown1.Value / 10;
+                if (started)
+                    Timer.Start();
+            }
+        }
+
+        private int LastHighlightStart;
+        private int LastHighlightLength;
+        private void HighlightCode(string code)
+        {
+            richTextBox1.Select(LastHighlightStart, LastHighlightLength);
+            richTextBox1.SelectionColor = richTextBox1.ForeColor;
+            int start = richTextBox1.Find(code);
+            LastHighlightStart = start;
+            LastHighlightLength = code.Length;
+            richTextBox1.Select(start, code.Length);
+            richTextBox1.SelectionColor = Color.Red;
+            richTextBox1.DeselectAll();
+        }
+
+        private void pausebtn_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+        }
+
+        private void resumebtn_Click(object sender, EventArgs e)
+        {
+            Timer.Start();
+        }
+    }
+}
